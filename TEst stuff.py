b5edc03c5359b5a6ba139f65ee38579cb1eb2acf
AudreyVersteegen <audrey@versteegen.us>
2022-04-04 10:46:25 -0400

    writing into file is looking good. it'll be tweaked in the actual code, of course

--- TEst stuff.py
+++ TEst stuff.py
@@ -18,5 +18,14 @@ print(resource)
 print(response_data)
 # here response_data is a list of dictionaries, each entry in the list is for a different provider
 # im just here to test run the code, give an example of it working
+# each provider has a numeric id, which will be used for calling for all movie data.
 # you might notice that on line 15 there is no parameter "header". I don't know if it is vital to include this, but
 # in this case it works.
+
+f = open("test_data.txt", "w")
+# param "w" will create a file, if there isnt already one b the given name
+# IT WILL ALSO OVERWRITE THE PREVIOUS TEXT IN THE FILE WHEN OU WRITE TO IT
+# we can use "a" (like "append") instead of "w" which wont overwrite the data, instead add to the end.
+for item in response_data:
+    f.write(item["strId"] +" " + str(item["id"]) + "\n")
+    # there's more data than just this, but i figured this is the bare bones needed.
