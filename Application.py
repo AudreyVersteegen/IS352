ebc39b331c90d3f950d81f77800d326753abe149
MikePass <michaellp@me.com>
2022-04-19 18:31:46 -0400

    search by title and runtime done

--- Application.py
+++ Application.py
@@ -13,14 +13,49 @@ def printMenu():
         userInput = printMenu()
     return userInput
 
+def searchMenu():
+    print("What would you like to search for?")
+    print("1. Title \n2. Runtime \n3. Release Date \n4. Language")
+    toSearch = input("Enter option:")
+    if toSearch == '1':
+        title = input("Enter title: ")
+        search(title, 't')
+    elif toSearch == '2':
+        # COME BACK TO THIS
+        # DO WE WANT TO HAVE A RUNTIME RANGE
+        print("Whould you like a movie that is: ")
+        print("1. Less then an hour\n2. In between 1 and 2 hours\n3. Longer then 2 hours")
+        runtime = input("Enter: ")
+        search(runtime, 'r')
+    elif toSearch == '3':
+        # COME BACK TO THIS
+        # DO WE WANT DECADE RANGES
+        date = input("Enter date: ")
+        search(date, 'd')
+    elif toSearch == 'l':
+        lang = input("Enter language: ")
+        search(lang, 'l')
+
 def search(input, flag): #flag can be t, r, d
     if flag == 't':
         #search by title
-        print("title")
-    if flag == 'r':
+        for title in movieDict.keys():
+            if input.upper() in title.upper():
+                # good
+                print(title)
+        for title in showDict.keys():
+            if input.upper() in title.upper():
+                print(title)
+    elif flag == 'r':
         # search by runtime
-        print("runtime")
-    if flag == 'd':
+        for item in movieDict:
+            if input == "1" and movieDict[item]['runtime'] < 60:
+                print(item)
+            elif input == "2" and (movieDict[item]['runtime'] >= 60 and movieDict[item]['runtime'] < 120):
+                print(item)
+            elif input == '3' and movieDict[item]['runtime'] > 120:
+                print(item)
+    elif flag == 'd':
         print("date")
         #search by release Date
     if flag == 'l':
@@ -54,7 +89,12 @@ with open(fileName) as filePointer:
 for movie in movies:
     elements = movie.split('\t')
     releaseDate = elements[1][-4:]
-    toAdd = {'releaseDate' : releaseDate, 'runtime' : elements[2], 'language' : elements[3]}
+    runtime = elements[2].strip()
+    if runtime != 'None':
+        runtime = int(runtime)
+    else:
+        runtime = 0
+    toAdd = {'releaseDate' : releaseDate, 'runtime' : runtime, 'language' : elements[3]}
     if elements[0] not in movieDict:
         movieDict[elements[0]] = toAdd
 
@@ -68,10 +108,7 @@ while True:
     if userInput == "1":
         # search for the movie with given parameters
         # ask for them
-        print("What would you like to search for?")
-        print("1. Title \n2. Runtime \n3. Release Date \n4. Language")
-        toSearch = input("enter stuff here")
-        print("searching....")
+        searchMenu()
     if userInput == "2":
         # show them their watchlist
         print("got to watchlist")
