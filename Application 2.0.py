1374a25b1f27dea2c48bff8473d33793bd899068
AudreyVersteegen <audrey@versteegen.us>
2022-04-22 10:23:33 -0400

    search release date is cleaned up. should be able to refine searces. untested

--- Application 2.0.py
+++ Application 2.0.py
@@ -1,19 +1,6 @@
 import pprint
-from datetime import datetime
 import openpyxl
-
-#setting up the language
-wkbk = openpyxl.load_workbook("is352 language code.xlsx")
-sheet = wkbk.active
-langDict = {}
-for line in range(2, sheet.max_row):
-    code = sheet.cell(row=line, column=1).value
-    lang = sheet.cell(row=line, column=2).value
-    langDict[code] = lang
-langDict['cn'] = 'Chinese'
-
 # read watchlist
-# search function
 Application = None
 
 
@@ -26,40 +13,46 @@ def printMenu():
         userInput = printMenu()
     return userInput
 
+# search functions
+def searchMenu():
+    mDict = mediaDict
+    searching = True
+    while searching:
+        print("What would you like to search for?")
+        print("1. Title \n2. Runtime \n3. Release Date \n4. Language")
+        toSearch = input("Enter option:")
+        if toSearch == '1':
+            title = input("Enter title: ")
+            mDict = search(title, 't', mDict)
+        elif toSearch == '2':
+            # COME BACK TO THIS
+            # DO WE WANT TO HAVE A RUNTIME RANGE
+            print("Would you like a movie that is: ")
+            print("1. Less then an hour\n2. In between 1 and 2 hours\n3. Longer then 2 hours")
+            runtime = input("Enter: ")
+            mDict = search(runtime, 'r', mDict)
+        elif toSearch == '3':
+            print("Select the range the release year falls within : ")
+            print("1. Before 1920\n2. After 1920 and before 1950 \n3. The 1950's\n4. The 1960's\n"
+                  "5. The 1970's\n6. The 1980's\n7. The 1990's\n8. The 2000's\n9. The 2010's\n10. The 2020's")
+            date = input("Enter: ")
+            mDict = search(date, 'd', mDict)
+        elif toSearch == '4':
+            lang = input("Enter language: ")
+            mDict = search(lang, 'l', mDict)
+        else:
+            print("That was not an option. Please enter a new command.")
+        print(mDict.keys())
+        yn = input('Would you like to narrow down these results? [y/n] ').strip().lower()
+        if yn != 'y':
+            searching = False
 
-def searchMenu(mDict):
 
-    print("What would you like to search for?")
-    print("1. Title \n2. Runtime \n3. Release Date \n4. Language")
-    toSearch = input("Enter option:")
-    if toSearch == '1':
-        title = input("Enter title: ")
-        results = search(title, 't', mDict)
-    elif toSearch == '2':
-        # COME BACK TO THIS
-        # DO WE WANT TO HAVE A RUNTIME RANGE
-        print("Would you like a movie that is: ")
-        print("1. Less then an hour\n2. In between 1 and 2 hours\n3. Longer then 2 hours")
-        runtime = input("Enter: ")
-        results = search(runtime, 'r', mDict)
-    elif toSearch == '3':
-        print("Select the range the release year falls within : ")
-        print("1. Before 1920\n2. After 1920 and before 1950 \n3. The 1950's\n4. The 1960's\n5. The 1970's\n6. The 1980's\n7. The 1990's\n8. The 2000's\n9. The 2010's\n10. The 2020's")
-        date = input("Enter: ")
-        results = search(date, 'd', mDict)
-    elif toSearch == '4':
-        lang = input("Enter language: ")
-        results = search(lang, 'l', mDict)
-    else:
-        print("That was not an option. Please enter a new command.")
-        searchMenu(mediaDict)
-    print(results.keys())
-
-
-def search(input, flag, mDict): #flag can be t, r, d
+def search(input, flag, mDict):  # flag can be t, r, d
     results = {}
+    input = input.strip()
     if flag == 't':
-        #search by title
+        # search by title
         for title in mDict.keys():
             if input.upper() in title.upper():
                 results[title] = mDict[title]
@@ -75,24 +68,14 @@ def search(input, flag, mDict): #flag can be t, r, d
                     results[item] = mDict[item]
     elif flag == 'd':
         # search by release date
-        start = 0
-        end = 0
-        if input == "1":
-            end = 1920
-        elif input == "2":
-            start = 1920
-            end = 1950
-        elif input in "3,4,5,6,7,8,9":
-            #not really sure that this'll run the way i want
-            start = 1920 + (int(input) * 10)
-            end = start + 10
-        else:
-            start = 2020
-            end = 10000
-        for item in mDict:
-            date = mDict[item]['releaseDate']
-            if date in range(start, end):
-                results[item] = mDict[item]
+        eras = [0, 1920, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, 2030]
+        if input in ["1", "2", "3", '4', '5', '6', '7', '8', '9', '10']:
+            for item in mDict:
+                start = eras[int(input) - 1]
+                end = eras[int(input)]
+                date = mDict[item]['releaseDate']
+                if date in range(start, end):
+                    results[item] = mDict[item]
     elif flag == 'l':
         # search by language
         for item in mDict:
@@ -110,6 +93,17 @@ def search(input, flag, mDict): #flag can be t, r, d
 # def writeWatchlist():
 #     # write at the end of running
 
+
+# setting up the language
+wkbk = openpyxl.load_workbook("is352 language code.xlsx")
+sheet = wkbk.active
+langDict = {}
+for line in range(2, sheet.max_row):
+    code = sheet.cell(row=line, column=1).value
+    lang = sheet.cell(row=line, column=2).value
+    langDict[code] = lang
+langDict['cn'] = 'Chinese'
+
 # read in the data, into a dict.
 # shows
 fileName = "Show_Data.txt"
@@ -125,10 +119,9 @@ for show in shows:
         releaseDate = int(releaseDate)
     else:
         releaseDate = 0
-    toAdd = {'releaseDate' : releaseDate, 'misc' : elements[2], 'language' : elements[3]}
+    toAdd = {'releaseDate': releaseDate, 'misc': elements[2], 'language': elements[3]}
     if elements[0] not in mediaDict:
         mediaDict[elements[0]] = toAdd
-
 
 # movies
 fileName = "Movie_Data.txt"
@@ -148,10 +141,9 @@ for movie in movies:
         runtime = int(runtime)
     else:
         runtime = 0
-    toAdd = {'releaseDate' : releaseDate, 'runtime' : runtime, 'language' : elements[3]}
+    toAdd = {'releaseDate': releaseDate, 'runtime': runtime, 'language': elements[3]}
     if elements[0] not in mediaDict:
         mediaDict[elements[0]] = toAdd
-
 
 # watchlist
 # fileName = "Watchlist.txt"
@@ -163,7 +155,6 @@ EXIT = "3"
 userInput = printMenu()
 while True:
     if userInput == EXIT:
-
         break
     if userInput == "1":
         # search for the movie with given parameters
